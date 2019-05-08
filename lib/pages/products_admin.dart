eb413dba34780ac9b634b9ba94c8ee6ce6fcac12
svenlc <sven.lecann@live.fr>
2019-05-09 01:38:45 +0200

    Section 9 ended

--- lib/pages/products_admin.dart
+++ lib/pages/products_admin.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
-import './product_create.dart';
+import './product_edit.dart';
 import './product_list.dart';
 
 class ProductsAdminPage extends StatelessWidget {
   final Function addProduct;
+  final Function updateProduct;
   final Function deleteProducts;
+  final List<Map<String, dynamic>> products;
 
-  ProductsAdminPage(this.addProduct, this.deleteProducts);
+  ProductsAdminPage(
+      this.addProduct, this.updateProduct, this.deleteProducts, this.products);
 
   Widget _buildSideDrawer(BuildContext context) {
     return Drawer(
@@ -52,8 +55,8 @@ class ProductsAdminPage extends StatelessWidget {
         ),
         body: TabBarView(
           children: <Widget>[
-            ProductCreatePage(addProduct),
-            ProductListPage(),
+            ProductEditPage(addProduct: addProduct),
+            ProductListPage(products, updateProduct),
           ],
         ),
       ),
